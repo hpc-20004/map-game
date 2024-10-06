6ab13d8ccb9c53655da6a06f6122a098b0ace654
hpc-20004 <20004@haurakiplains.school.nz>
2024-10-06 19:35:52 +1300

    room location checking works, just not the L shaped rooms yet. also did some formatting

--- map_game.py
+++ map_game.py
@@ -12,6 +12,7 @@ Python: 3.11
 #imports
 import pygame, sys
 from pygame.locals import QUIT
+from walls import create_walls
 
 #setup
 pygame.init()
@@ -77,17 +78,12 @@ class Player:
         map_offset[0] += x
         map_offset[1] += y
         
-        # if -690 < map_offset[0] < -580 and 590 <= map_offset[1] <= 595:
-        #     if keys[pygame.K_c]:
-        #         print("tv found")
-                
         return self.moving
     
-#test this. maybe find something that calculates the players distance from tv instead using coordinates
-
 #game item class
 class Item:
-    def __init__(self, min_x_offset, max_x_offset, min_y_offset, max_y_offset, floor, found):
+    def __init__(self, name, min_x_offset, max_x_offset, min_y_offset, max_y_offset, floor, found):
+        self.name = name
         self.min_x_offset = min_x_offset
         self.max_x_offset = max_x_offset
         self.min_y_offset = min_y_offset
@@ -100,8 +96,29 @@ class Item:
         
         if self.min_x_offset <= map_offset_x <= self.max_x_offset and self.min_y_offset <= map_offset_y <= self.max_y_offset:
             if keys[pygame.K_c]:
-                print("{} found".format(self))
+                print("{} found".format(self.name))
                 self.found = True
+                
+#room class
+class Room:
+    def __init__(self, name, min_x_offset, max_x_offset, min_y_offset, max_y_offset, floor, locked, in_room):
+        self.name = name
+        self.min_x_offset = min_x_offset
+        self.max_x_offset = max_x_offset
+        self.min_y_offset = min_y_offset
+        self.max_y_offset = max_y_offset
+        self.floor = floor # once floors are implemented in the find items method check if player's floor and room floor are the same
+        self.locked = locked
+        self.in_room = in_room
+        
+    def check_room_location(self, map_offset_x, map_offset_y, previous_player_location):
+        if self.min_x_offset <= map_offset_x <= self.max_x_offset and self.min_y_offset <= map_offset_y <= self.max_y_offset:
+            self.in_room = True
+            return self.name
+        else:
+            self.in_room = False
+            return previous_player_location # if they're still in the same room?
+
 #wall class
 class Wall:
     def __init__(self, x, y, width, height):
@@ -129,20 +146,37 @@ MAP_Y = -50
 collide = False
 thief_frame = 0
 moving = False
+player_location = 0
 
 #item list
 item_list = []
 
-book_item = Item(525,580,195,205,1,False)
+book_item = Item("The Great Gutsby", 525,580,195,205,1,False)
 item_list.append(book_item)
 
-master_key_item = Item(265,285,545,600,1,False)
+master_key_item = Item("Master Bedroom Key",265,285,545,600,1,False)
 item_list.append(master_key_item)
 
-tv_item = Item(-690,)
+tv_item = Item("TV",-690,-580,590,595,1,False)
+item_list.append(tv_item)
+
+#room list
+room_list = []
+
+foyer = Room("Foyer",-240,285,-180,410,1,False,False)
+room_list.append(foyer)
+
+library = Room("Library",240,635,-50,205,1,False,False)
+room_list.append(library)
+
+dining = Room("Dining Room",335,635,305,655,1,False,False)
+room_list.append(dining)
+
+cleaning = Room("Cleaning Supplies Closet",110,285,495,610,1,False,False)
+room_list.append(cleaning)
 
 #player
-thief_sprites = []
+THIEF_SPRITES = []
 for sprite in range(12):  #tile000.png to tile011.png
     
     #image name
@@ -162,12 +196,12 @@ for sprite in range(12):  #tile000.png to tile011.png
     resized_sprite = pygame.transform.scale(original_sprite, new_size)
         
     #append to sprite list
-    thief_sprites.append(resized_sprite)
+    THIEF_SPRITES.append(resized_sprite)
 
-thief = Player(thief_sprites[8],450, 300, 50, 50, 5,moving) #x and y values are in the center but this places the thief 'top left' in the middle
+thief = Player(THIEF_SPRITES[8],450, 300, 50, 50, 5,moving) #x and y values are in the center but this places the thief 'top left' in the middle
 # thief.rect.center = screen_rect.center
 
-current_sprite = thief_sprites[8]
+current_sprite = THIEF_SPRITES[8]
 
 #set up tiles?
 TILE_SIZE = 20
@@ -177,100 +211,11 @@ floor_1_surface = pygame.image.load('floor 1.png').convert()
 floor_1_rect = floor_1_surface.get_rect(center = (540,-50)) #initially puts center at the screen center
 
 #screen's center offset
-center_offset_x = screen_rect.centerx -30
-center_offset_y = screen_rect.centery +355
+CENTER_OFFSET_X = screen_rect.centerx -30
+CENTER_OFFSET_Y = screen_rect.centery +355
 
 #walls (relative to the center of the screen)
-wall_list = []
-
-#library
-lib_top = wall_list.append(Wall(205 - center_offset_x, 675 - center_offset_y, 655-205, 10))
-lib_right_top = wall_list.append(Wall(650 - center_offset_x, 675 - center_offset_y, 10, 850-675))
-lib_right_bottom = wall_list.append(Wall(650 - center_offset_x, 920 - center_offset_y, 10, 1030-920))
-lib_bottom = wall_list.append(Wall(205 - center_offset_x, 1030 - center_offset_y, 685-205, 10))
-#       furniture
-#                       reading
-lib_couch = wall_list.append(Wall(465 - center_offset_x,820 - center_offset_y, 85, 5))
-lib_table = wall_list.append(Wall(485 - center_offset_x,880 - center_offset_y, 50,5))
-lib_stool_1 = wall_list.append(Wall(530 - center_offset_x, 940 - center_offset_y, 15, 5))
-lib_stool_2 = wall_list.append(Wall(465 - center_offset_x, 940 - center_offset_y, 15, 5))
-lib_shelf = wall_list.append(Wall(215 - center_offset_x, 720 - center_offset_y,655-220, 5))
-#                       desk 
-lib_chair = wall_list.append(Wall(465-150 - center_offset_x, 840 - center_offset_y, 40, 5))
-lib_desk = wall_list.append(Wall(280 - center_offset_x, 884 - center_offset_y, 100, 10))
-lib_plant_top = wall_list.append(Wall(280-70 - center_offset_x, 884+90 - center_offset_y, 50, 5))
-lib_plant_right = wall_list.append(Wall(260 - center_offset_x, 884+90 - center_offset_y, 5, 50))
-
-
-#dining
-din_top = wall_list.append(Wall(210 - center_offset_x, 195 - center_offset_y, 560-205, 10))
-din_lib_left = wall_list.append(Wall(205 - center_offset_x, 200 - center_offset_y, 10, 1030-200))
-din_right_top = wall_list.append(Wall(555 - center_offset_x, 195 - center_offset_y, 10, 530-195))
-din_right_bottom = wall_list.append(Wall(555 - center_offset_x, 600 - center_offset_y, 10, 675-600))
-#       furniture
-dining_wall = wall_list.append(Wall(210 - center_offset_x, 540-95-115-20-50+5 - center_offset_y, 560-205, 10))
-dining_chair_1 = wall_list.append(Wall(1100-190+5-120-50-180-240 - center_offset_x,540-95-115-20-50+50+70+50 - center_offset_y, 25,5))
-dining_table = wall_list.append(Wall(1100-190+5-120-50-180-275 - center_offset_x,540-95-115-20-50+50+70+70 - center_offset_y, 100,40))
-dining_chair_2 = wall_list.append(Wall(1100-190+5-120-50-180-240 - center_offset_x,540-95-115-20-50+50+70+50+80 - center_offset_y, 25,5))
-
-
-#cleaning room
-cleaning_right = wall_list.append(Wall(780 - center_offset_x, 135 - center_offset_y, 10, 485-135))
-cleaning_top = wall_list.append(Wall(555 - center_offset_x, 260 - center_offset_y, 785-560, 10))
-cleaning_bottom_left = wall_list.append(Wall(560 - center_offset_x, 485 - center_offset_y, 640-560, 10))
-#       furniture
-cleaning_wall = wall_list.append(Wall(1100-190+5-120-50-180 - center_offset_x,540-95-115-20-50+50 - center_offset_y, 785-560,10))
-cleaning_bench_1 = wall_list.append(Wall(1100-190+5-120-50-180 - center_offset_x,540-95-115-20-50+50+70 - center_offset_y, 20,5))
-cleaning_bench_2 = wall_list.append(Wall(1100-190+5-120-50-180+120 - center_offset_x,540-95-115-20-50+50+70 - center_offset_y, 100,5))
-
-
-#kitchen
-kit_cle_bottom = wall_list.append(Wall(710 - center_offset_x, 485 - center_offset_y, 1020-710, 10))
-kit_bottom_mid = wall_list.append(Wall(1090 - center_offset_x, 485 - center_offset_y, 1135-1090, 10))
-kit_right_top = wall_list.append(Wall(1100 - center_offset_x, 135 - center_offset_y, 10, 265-100))
-kit_liv_top = wall_list.append(Wall(1100 - center_offset_x, 260 - center_offset_y, 1645-1100, 10))
-kit_bottom_right = wall_list.append(Wall(1135 - center_offset_x, 710 - center_offset_y, 1365-1135, 10))
-kit_left_top = wall_list.append(Wall(1130 - center_offset_x, 485 - center_offset_y, 10, 565-485))
-kit_top = wall_list.append(Wall(780 - center_offset_x, 135 - center_offset_y, 1100-780,10))
-kit_left_bottom = wall_list.append(Wall(1130 - center_offset_x, 630 - center_offset_y, 10, 810-630))
-#       furniture
-kitchen_island = wall_list.append(Wall(1100-190+5 - center_offset_x, 540-95-115-20 - center_offset_y, 90, 5))
-fridge = wall_list.append(Wall(1100-190+5-120 - center_offset_x,540-95-115-20-50 - center_offset_y, 40, 40))
-kitchen_wall = wall_list.append(Wall(1100-190+5-120-10 - center_offset_x, 540-95-115-20-50-60 - center_offset_y,1100-780,10))
-
-
-#living area
-living_left_top = wall_list.append(Wall(1355 - center_offset_x, 265 - center_offset_y, 10, 715-265))
-living_left_bottom = wall_list.append(Wall(1130 - center_offset_x, 965 - center_offset_y, 10, 1030-965))
-living_right = wall_list.append(Wall(1645 - center_offset_x, 265 - center_offset_y, 10, 1030-265))
-living_bottom = wall_list.append(Wall(1105 - center_offset_x, 1030 - center_offset_y, 1645-1105, 10))
-#       furniture
-living_paintings = wall_list.append(Wall(1130 - center_offset_x, 740 - center_offset_y, 1360-1130, 5))
-fire_couch = wall_list.append(Wall(1510 - center_offset_x, 680 - center_offset_y, 105, 10))
-fireplace = wall_list.append(Wall(1510 - center_offset_x, 540 - center_offset_y, 95, 40))
-tv_couch = wall_list.append(Wall(1510-60 - center_offset_x, 540-95 - center_offset_y, 105, 10))
-tv_wall = wall_list.append(Wall(1100 - center_offset_x, 540-95-115 - center_offset_y, 1645-1100,5))
-tv = Wall(1455 - center_offset_x,330 - center_offset_y, 100, 10)
-wall_list.append(tv)
-#                   conversation pit
-conversation_pit_top = wall_list.append(Wall(1360 - center_offset_x, 840 - center_offset_y, 250, 5))
-conversation_pit_left = wall_list.append(Wall(1360 - center_offset_x, 915 - center_offset_y, 5, 55))
-conversation_pit_stair = wall_list.append(Wall(1360 - center_offset_x, 915 - center_offset_y, 25, 5))
-conversation_pit_bottom = wall_list.append(Wall(1360 - center_offset_x, 970 - center_offset_y, 250, 5))
-conversation_pit_right = wall_list.append(Wall(1610 - center_offset_x, 840 - center_offset_y, 5, 130))
-con_table = wall_list.append(Wall(1450 - center_offset_x, 900 - center_offset_y, 80, 5))
-con_chair = wall_list.append(Wall(1590 - center_offset_x, 880 - center_offset_y, 15, 5))
-
-
-#foyer
-foyer_left = wall_list.append(Wall(685 - center_offset_x, 1030 - center_offset_y, 10, 1160-1030))
-foyer_right = wall_list.append(Wall(1105 - center_offset_x, 1030 - center_offset_y, 10, 1160-1030))
-foyer_bottom_left = wall_list.append(Wall(685 - center_offset_x, 1160 - center_offset_y, 860-685, 10))
-foyer_bottom_right = wall_list.append(Wall(925 - center_offset_x, 1160 - center_offset_y, 1105-925, 10))
-
-#stair walls
-stair_1_left = wall_list.append(Wall(800 - center_offset_x, 485 - center_offset_y, 10, 725-485))
-stair_1_right = wall_list.append(Wall(960 - center_offset_x, 485 - center_offset_y, 10, 725-485))
+wall_list = create_walls(Wall, CENTER_OFFSET_X, CENTER_OFFSET_Y)
 
 #starting map offset
 map_offset = [0, 0]  #x offset, y offset
@@ -289,6 +234,7 @@ while True:
             print(pygame.mouse.get_pos())  # for checking coordinates
             print(map_offset[0])
             print(map_offset[1])
+            print(player_location)
 
         if event.type == ANIMATION:
             if keys[pygame.K_w]:
@@ -302,12 +248,17 @@ while True:
 
     moving = thief.player_movement(wall_list, map_offset)
     
+    #check if any items are found
     for item in item_list:
         item.find_items(map_offset[0],map_offset[1])
-    
+        
+    #check what room the player's in
+    for room in room_list:
+        player_location = room.check_room_location(map_offset[0],map_offset[1],player_location)
+        
 
     # update the current sprite based on the animation frame
-    current_sprite = thief_sprites[thief_frame]
+    current_sprite = THIEF_SPRITES[thief_frame]
     thief.surface = current_sprite
 
     # update floor position
